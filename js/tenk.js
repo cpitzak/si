52a24cb076829a2d833c34a8ea3741728dc4da80
Clint <cpitzak@gmail.com>
2019-12-17 06:24:09 -0800

    Update buffett calcs

--- js/tenk.js
+++ js/tenk.js
@@ -24,17 +24,30 @@ var tenk = {
       results[0].push(data[0][i]);
     }
   },
-  calcIncome: function(data, results) {
-    if (!data || !results) {
+  calcIncome: function(incomeData, balanceData, cashData, results) {
+    if (!incomeData || !results) {
       return;
     }
-    for (let i = 1; i < data[0].length; i++) {
+    for (let i = 1; i < incomeData[0].length; i++) {
       // gross profit margin
-      const revenue = parseFloat(data[1][i]);
-      const cogs = parseFloat(data[3][i]);
+      const revenue = parseFloat(incomeData[1][i]);
+      const cogs = parseFloat(incomeData[3][i]);
       const grossProftMargin = (revenue - cogs) / revenue;
       const gpmPercent = Math.round(grossProftMargin * 10000) / 100;
       results[1].push(gpmPercent + '%');
+      // SGA / Gross Profit
+      const sga = parseFloat(incomeData[6][i]);
+      const grossProfit = parseFloat(incomeData[4][i]);
+      const sgaByGrossProfit = Math.round((sga / grossProfit) * 10000) / 100;
+      results[2].push(sgaByGrossProfit + '%');
+      // R&D / Gross Profit
+      const rd = parseFloat(incomeData[5][i]);
+      const rdByGrossProfit = Math.round((rd / grossProfit) * 10000) / 100;
+      results[3].push(rdByGrossProfit + '%');
+      // Depreciation / Gross Profit
+      const depreciation = parseFloat(cashData[1][i]);
+      const depreciationByGrossProfit = Math.round((depreciation / grossProfit) * 10000) / 100;
+      results[4].push(depreciationByGrossProfit + '%');
     }
   },
   buildTable: function(ticker, data) {
@@ -73,12 +86,18 @@ var tenk = {
     const cashData = await tenk.load(cashUrl);
     const results = [
       ['Name', 'Computation', 'Desired'],
-      ['Gross Profit Margin', '(Revenue - COGS) / Revenue', '> 40% for past 10 years']
+      ['Gross Profit Margin', '(Revenue - COGS) / Revenue', '> 40% for past 10 years'],
+      ['', 'SGA / Gross Profit', '< 80% is okayish; < 30% is great; consistent over time'],
+      ['', 'R&D / Gross Profit', 'Depends; < 30% seems good'],
+      ['', 'Depreciation / Gross Profit', '< 10%, but depends on industry'],
     ];
     tenk.addYearHeaders(incomeData, results);
-    tenk.calcIncome(incomeData, results);
-    console.dir(incomeData);
-    console.dir(results);
+    tenk.calcIncome(incomeData, balanceData, cashData, results);
     tenk.buildTable(ticker, results);
+  },
+  onKeyUpCalc: async function(event) {
+    if (event.keyCode === 13) {
+      tenk.calc(document.getElementById('ticker').value);
+    }
   }
 };
